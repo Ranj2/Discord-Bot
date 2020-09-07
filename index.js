de75ed813adb83ea18e08983beaf609209b7f9ea
Ranj2 <ranj.313185016@uhd.edu.iq>
2020-09-07 10:48:07 +0300

    comit1

---
+++ index.js
@@ -0,0 +1,76 @@
+const Discord = require('discord.js')
+const cheerio = require('cheerio')
+const request = require('request')
+const Bot = new Discord.Client()
+const prefix = '-'
+
+//when the bot is called
+Bot.on('message', (message)=>{
+    
+    if(!message.content.startsWith(prefix)|| message.author.bot)return;
+
+    //slicing the command into a string array
+    const args = message.content.slice(prefix.length).split(" ")
+    
+    
+    if(args[0]==='meme'){
+        image(args[0])   
+    }
+    if(args[0]===''){
+
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function image(message){
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + `${message}`,
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Chrome"
+           }
+
+        };
+        request(options, function(error, response, responseBody) {
+            if (error) {
+                return;
+            }
+  
+  
+            $ = cheerio.load(responseBody);
+  
+  
+            var links = $(".image a.link");
+  
+            var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+  
+            console.log(urls);
+  
+            if (!urls.length) {
+                return;
+            }
+  
+            // Send result
+            message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
+        });
+}
+
+
+Bot.login('NzUyMTM1MTUwOTg4Njg5NDIw.X1TOcQ.vt2T9dJzYks-IZNdxGbpDF1KQOw')
